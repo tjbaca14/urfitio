81f6d31bd972e06f7938a615a8c586db12faf126
Tucker Baca <tj.baca14@gmail.com>
2026-01-22 16:50:37 -0700

    cleanup

--- app/chat/repository/__init__.py
+++
@@ -1,7 +0,0 @@
-from .chat_repository import ChatRepository
-from .feedback_repository import FeedbackRepository
-
-__all__ = [
-    "ChatRepository",
-    "FeedbackRepository",
-]
--- app/coaches/__init__.py
+++
@@ -1,9 +0,0 @@
-from .repository import CoachRepository
-from .routes import schools_router
-from .service import CoachService
-
-__all__ = [
-    "CoachService",
-    "schools_router",
-    "CoachRepository",
-]
