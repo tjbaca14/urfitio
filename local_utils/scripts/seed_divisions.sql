3417d0ef357e0a16fa298a16697d408b49ded1b4
Tucker Baca <tj.baca14@gmail.com>
2026-01-28 17:55:14 -0700

    rm local files

--- local_utils/scripts/seed_divisions.sql
+++
@@ -1,9 +0,0 @@
--- Seed divisions
--- Generated from data/index.json
-
-INSERT INTO app.division (id, division_type)
-VALUES
-    ('d1', 'Division I'),
-    ('d2', 'Division II'),
-    ('d3', 'Division III')
-ON CONFLICT (id) DO NOTHING;
--- local_utils/server/configure.sh
+++
@@ -1,3 +0,0 @@
-sudo nano /etc/nginx/conf.d/urfit.io.conf; # edit nginx 
-sudo nginx -t; # test nginx conf
-sudo systemctl reload nginx; # reload nginx
